9d4b580fb3e8c8904aa7e975930fc817c5ca511e
Judah Doupe <judahdoupe@gmail.com>
2025-01-08 19:01:18 -0500

    added bevy basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_systems(Startup, setup_board)
+    .add_systems(Update, (sanity, count_tiles))
+    .run();
+}
+
+fn sanity() {
+    println!("running");
+}
+
+#[derive(Component)]
+struct Tile;
+
+#[derive(Component)]
+struct Element(String);
+
+fn setup_board(mut commands: Commands){
+    commands.spawn((Tile, Element("Fire".to_string())));
+    commands.spawn((Tile, Element("Earth".to_string())));
+    commands.spawn((Tile, Element("Water".to_string())));
+    commands.spawn((Tile, Element("Fire".to_string())));
+}
+
+fn count_tiles(query: Query<&Element, With<Tile>>){
+    for element in &query{
+        println!("tile is {}", element.0);
+    }
 }
